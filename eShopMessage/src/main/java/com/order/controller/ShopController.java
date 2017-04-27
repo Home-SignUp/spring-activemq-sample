1637d0b7c919e77736712700e3b6a8aea17a50c5
Sasha <skmets@gmail.com>
2017-04-27 18:28:31 +0300

    eShop_Spring+JMS+ActiveMQ+DB_xml

--- eShopMessage/src/main/java/com/order/controller/ShopController.java
+++ eShopMessage/src/main/java/com/order/controller/ShopController.java
@@ -1,16 +1,39 @@
 package com.order.controller;
 
-import org.springframework.stereotype.Controller;
+import com.order.dao.UserDao;
+import com.order.model.User;
 import org.springframework.ui.ModelMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class ShopController {
 
-	@RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
-	public String prepareProduct(ModelMap model) {
-		return "confirmOrder";
-	}
+	private static final Logger logger = LoggerFactory.getLogger(ShopController.class);
 
+	@Autowired
+	UserDao userDao;
+
+    @RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
+    public String prepareProduct(ModelMap model) {
+        return "confirmOrder";
+    }
+
+	@RequestMapping(value = "/db", method = RequestMethod.GET)
+	public String welcome(Model model) {
+		logger.debug("order");
+
+		List<User> users = userDao.findAll(); //User user = userDao.findByName("order");
+		System.out.println(users);
+		model.addAttribute("user", users);
+
+		return "orderData";
+	}
 }
